429687a2fb07af9b1833d7fddf20e125a8f9e2a5
Esteban SEMELLIER <esteban.semellier@gmail.com>
2022-11-09 14:02:28 +0100

    ajout contentView

--- RecipeGroupAA/ContentView.swift
+++ RecipeGroupAA/ContentView.swift
@@ -4,17 +4,31 @@
 //
 //  Created by Amelie Pocchiolo on 04/11/2022.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("")
+        TabView{
+            HomeView()
+                .tabItem{
+                    Label("Recettes", systemImage: "list.dash")
+                }
+//            FavoriView()
+//                .tabItem{
+//                    Label("Favoris", systemImage: "heart")
+//                }
+//
+//            UserView()
+//                .tabItem{
+//                    Label("Profile", systemImage: "person.fill")
+//
+//                } A RAJOUTER A L'ASSEMBLAGE DES FICHIERS
         }
-        .padding()
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
