f40a7fcffbe817ff847e8e49c699a0d9dff7f977
Esteban SEMELLIER <esteban.semellier@gmail.com>
2022-11-09 13:55:52 +0100

    Correction merge

--- RecipeGroupAA/View/AddRecipeView.swift
+++ RecipeGroupAA/View/AddRecipeView.swift
@@ -70,6 +70,7 @@ struct AddRecipeView: View {
 struct AddRecipeView_Previews: PreviewProvider {
     static var previews: some View {
         AddRecipeView()
+            .environmentObject(AddRecipeViewModel())
     }
 }
 
